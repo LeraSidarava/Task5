3fe7cd6acaaea123be60f659af389730a2321320
lerasidarava <lera.sidarava.j@gmail.com>
2023-03-17 17:41:11 +0100

    added task 5 functionality

---
+++ Task_5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApplicationSecondMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a series of numbers separated by a space:");
+
+            string input = Console.ReadLine();
+
+            int[] numbers = input.Split(',').Select(int.Parse).ToArray();
+
+            //Declare max and seconMax values
+
+            int max = int.MinValue;
+            int secondMax = int.MinValue;
+
+            //Find secondMax value by 
+
+
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    secondMax = max;
+                    max = number;
+                }
+
+                else if (number > secondMax && number < max)
+                {
+                    secondMax = number;
+                }
+            }
+
+
+
+
+            Console.WriteLine("The second maximum value is: " + secondMax);
+
+            Console.ReadLine();
+        }
+    }
+}
